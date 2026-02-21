063f143638ca86c69a2750c0fcfb1106d70008bb
Damon  Lu <59256766+WhatDamon@users.noreply.github.com>
2026-02-22 04:30:41 +0800

    Add architecture check

--- composeApp/src/jvmMain/kotlin/com/lanrhyme/micyou/platform/PlatformInfo.kt
+++ composeApp/src/jvmMain/kotlin/com/lanrhyme/micyou/platform/PlatformInfo.kt
@@ -18,6 +18,24 @@ object PlatformInfo {
     val isWindows: Boolean get() = currentOS == OS.WINDOWS
     val isLinux: Boolean get() = currentOS == OS.LINUX
     val isMacOS: Boolean get() = currentOS == OS.MACOS
+
+    enum class Arch {
+        X86_64, ARM64, X86, OTHER
+    }
+
+    val currentArch: Arch by lazy {
+        val arch = System.getProperty("os.arch", "").lowercase()
+        when {
+            arch == "x86_64" || arch == "amd64" || arch == "x64" -> Arch.X86_64
+            arch == "aarch64" || arch == "arm64" -> Arch.ARM64
+            arch == "x86" || arch == "i386" || arch == "i486" || arch == "i586" || arch == "i686" -> Arch.X86
+            else -> Arch.OTHER
+        }
+    }
+
+    val isX64: Boolean get() = currentArch == Arch.X86_64
+    val isX86: Boolean get() = currentArch == Arch.X86
+    val isArm64: Boolean get() = currentArch == Arch.ARM64
     
     val osName: String get() = System.getProperty("os.name", "Unknown")
     val osVersion: String get() = System.getProperty("os.version", "Unknown")
